4663aa79c279c296b558229a341bb699727acfba
Hadson Roque <nineonezero.light@gmail.com>
2023-04-20 16:06:47 +0200

    String di connessione

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace adonet_db_videogame
+﻿using System.Data.SqlClient;
+
+namespace adonet_db_videogame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Apertura connessione
+            string stringConnection = "Data Source=localhost;Initial Catalog=test;Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(stringConnection);      
         }
     }
 }
