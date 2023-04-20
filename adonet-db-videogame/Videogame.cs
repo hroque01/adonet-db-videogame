9e226e767674a3a1ac04d8218925817748d037ec
Hadson Roque <nineonezero.light@gmail.com>
2023-04-20 17:18:06 +0200

    Create InserisciVideogame function

--- adonet-db-videogame/Videogame.cs
+++ adonet-db-videogame/Videogame.cs
@@ -8,5 +8,23 @@ namespace adonet_db_videogame
 {
     internal class Videogame
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public DateTime Release_date { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Update_at { get; set; }
+        public int Software_house_id { get; set; }
+
+        public Videogame(int id, string name, string overview, DateTime release_date, DateTime created_at, DateTime update_at, int software_house_id)
+        {
+            Id = id;
+            Name = name;
+            Overview = overview;
+            Release_date = release_date;
+            Created_at = created_at;
+            Update_at = update_at;
+            Software_house_id = software_house_id;
+        }
     }
 }
--- adonet-db-videogame/VideogameManager.cs
+++ adonet-db-videogame/VideogameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,42 @@ namespace adonet_db_videogame
 {
     internal class VideogameManager
     {
+        private readonly string connectionString;
+
+        public VideogameManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void InserisciVideogame(Videogame videogame)
+        {
+            //istanzio l'using
+            using (SqlConnection  conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    var sql = $"INSERT INTO videogames (name, overview, release_date, created_at, update_at, software_house_id)" +
+                        $"OUTPUT INSERTED.ID" +
+                        $"VALUES (@Name, @Overview, @Release_date, @Creted_at, @Update_at, Software_house_id)";
+                    using(SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Name", videogame.Name);
+                        cmd.Parameters.AddWithValue("@Overview", videogame.Overview);
+                        cmd.Parameters.AddWithValue("@Release_date", videogame.Release_date);
+                        cmd.Parameters.AddWithValue("@Creted_at", videogame.Created_at);
+                        cmd.Parameters.AddWithValue("@Update_at", videogame.Update_at);
+                        cmd.Parameters.AddWithValue("@Software_house_id", videogame.Software_house_id);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);  
+                }
+                
+
+
+            }
+        }
     }
 }
